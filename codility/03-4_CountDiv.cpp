2bb11ca25058e13cd47ec3307b0f2d1abe3cac4f
Keen <keen.liau@flightaware.com>
2014-09-18 14:47:11 -0500

    changed implementation for CountDiv

--- codility/03-4_CountDiv.cpp
+++ codility/03-4_CountDiv.cpp
@@ -1,23 +1,10 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
 
 // Lesson 3 - Prefix Sums
 // CountDiv
 // Compute number of integers divisible by k in range [a..b].
 
+#include <math.h>
 int solution(int A, int B, int K) {
-	int sol = ceil((double)(B - A) / K);
-	if (A % K == 0 && B % K == 0) sol++;
-	return sol;
-}
-
-int main() {
-	while(1) {
-		int A, B, C;
-		cin >> A >> B >> C;
-		cout << "Number of integers in [" << A << ", " << B << "] divisible by "
-			   << C << " = " << solution(A, B, C) << endl;
-	}
-	return 0;
+    double k = K;
+    return floor(B/k) - ceil(A/k) + 1;
 }
