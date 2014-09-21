1f5ed07250f7d0db5609d231e5f313482403d4a3
Keen <kyliau@umich.edu>
2014-09-21 17:14:42 -0500

    Finally a correct implementation for QuickSort

--- quicksort.cpp
+++ quicksort.cpp
@@ -5,57 +5,50 @@
 using namespace std;
 
 // generates a number that is in [min, max]
-unsigned pick_index(unsigned min, unsigned max) {
+// this serves as a place holder for better implementation, such as 
+// median of medians
+int pick_index(int min, int max) {
 	// simple implementation:
 	return min + (max - min) / 2;
 }
 
-void partition(vector<int>& A, unsigned left, unsigned right, unsigned index) {
+// partition the array A[left]..A[right] such that A[index] is in its 
+// final sorted position and all elements to the left are <= than A[index]
+// and all elements to the right are > than A[index]
+// (This algorithm is tricky and I got it wrong many times, in the end
+//  I referred to CLRS, so this is from CLRS with slight modification)
+int partition(vector<int>& A, int left, int right, int index) {
+
 	assert(index >= left && index <= right);
 
 	int target = A[index];
-	cout << "index = " << index << ", target = " << target << endl;
 
+  // place target in the right most position
 	swap(A[index],A[right]);
-	unsigned j = left;
-	for (unsigned i = left; i < right; i++) {
-		if (A[i] < target) {
-			swap(A[i], A[j]);
-			j++;
-		}
-	}
-	swap(A[j],A[right]);
-	/*
-	for (unsigned i = left; i <= right; i++) {
-		if (A[i] > target) {
-			swap(A[i], A[index]);
-			index = i;
-			break;
-		}
-	}
-	*/
-	for (int a : A) {
-		cout << a << " ";
-	}
-	cout << endl;
-	for (unsigned i = left; i <= right; i++) {
-		if (i <= j) {
-			assert(A[i] <= A[j]);
-		} else {
-			assert(A[i] > A[j]);
-		}
-	}
 
+  int i = left;
+  for (int j = left; j < right; ++j) {
+    if (A[j] <= target) {
+      swap(A[i], A[j]);
+      i++;
+    }
+  }
+  
+  swap(A[i], A[right]);
+
+  return i;
 }
 
 // sort the vector v from v[left] to v[right] (inclusive)
-void quicksort(vector<int>& A, unsigned left, unsigned right) {
+void quicksort(vector<int>& A, int left, int right) {
 
 	if (left >= right) return;
 
 	// randomly pick an element within [left, right]
-	unsigned index = pick_index(left, right);
-	partition(A, left, right, index);
+	int index = pick_index(left, right);
+
+	index = partition(A, left, right, index);
+
 	// at this point v[index] is at the correct location in the sorted array
 	// all elements to the left of A[index] are <= A[index], all elements to the
 	// right are > A[index]
