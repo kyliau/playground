ad1c84ffc76e08474709ffcd0b38b5b42910a03e
Keen Yee Liau <kyliau@umich.edu>
2014-09-25 17:43:16 -0500

    Create string_permutations.cpp

---
+++ string_permutations.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Print all possible permutations of a string
+// For example: Given the string 'abc', print
+// abc bac cab
+// acb bca cba
+
+// return a vector of all possible permutations of s
+vector<string> helper(const string& s) {
+
+  vector<string> result;
+  
+  if (s.length() == 0) {
+    return result;
+  }
+  
+  if (s.length() == 1) {
+    result.push_back(s);
+    return result;
+  }
+  
+  char c = s.back();
+  s.pop_back();
+  for (const string& p : helper(s)) {
+    // put c in every possible location
+    for (int i = 0; i < p.size() + 1; i++) {
+      result.push_back(p.substr(0, i) + c + p.substr(i));
+      cout << result.back() << endl;
+    }
+  }
+  
+  return result;
+}
+
+void string_permutations(const string& s) {
+  for (const string& p : helper(s)) {
+    cout << p << endl;
+  }
+}
+
+int main() {
+  for (const string& p : helper("abc")) {
+    cout << p << endl;
+  }
+  return 0;
+}
