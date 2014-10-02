ba29e36745a6df41329ae642521d385cebd47e6f
Keen <kyliau@umich.edu>
2014-10-02 16:14:24 -0400

    added MaxSliceSum and Anagram

---
+++ codility/7-3_MaxSliceSum.cpp
@@ -0,0 +1,20 @@
+
+// Lesson 7 - Maximum Slice Problem
+// MaxSliceSum
+// Find a maximum sum of a compact subsequence of array elements.
+
+#include <algorithm>
+int solution(vector<int> &A) {
+    
+    // check the case when all numbers are negative
+    int max_elem = *max_element(A.begin(), A.end());
+    if (max_elem < 0) return max_elem;
+    
+    int sum = 0, max_sum = 0;
+    for (int a : A) {
+        sum = max(0, sum + a);
+        max_sum = max(max_sum, sum);
+    }
+    return max_sum;
+
+}
