8fe9dd0fd2056f670ab053856b661d34fd58f7bf
Keen <kyliau@umich.edu>
2014-10-02 16:30:47 -0400

    consolidated two anagram implementations into anagram.cpp

--- anagram_old.cpp
+++ anagram_old.cpp
@@ -1,39 +1,11 @@
-#include <string>
-#include <unordered_map>
 #include <iostream>
+#include <string>
 using namespace std;
 
 // given two strings, write a function that returns true if they are anagrams
 // false otherwise
 
-// best implementation so far
-bool anagram(const string& s1, const string& s2) {
-
-  if (s1.length() != s2.length()) return false;
-
-  unordered_map<char, int> hash;
-
-  for (char c : s1) {
-    hash[c]++;
-  }
-
-  for (char c : s2) {
-    if (--hash[c] < 0) return false;
-  }
-
-  // this is not needed because of the length check above
-  /*
-  for (const auto& p : hash) {
-    if (p.second != 0) return false;
-  }
-  */
-
-  return true;
-
-}
-
 // assume both words contain alphabets [a-z][A-Z] and spaces only
-// implement without using hash table
 bool checkIsAnagram(const string& word1, const string& word2)
 {
   if (word1.length() != word2.length()) return false;
@@ -63,16 +35,13 @@ bool checkIsAnagram(const string& word1, const string& word2)
   return true;
 }
 
-int main() {
-  while (1) {
-    string s1, s2;
-    cout << "Enter first string: ";
-    cin.getline(s1);
-    cout << "Enter second string: ";
-    cin.getline(s2);
-    string result = anagram(s1, s2) ? " are " : " are not ";
-    cout << s1 << " and " << s2 << result << "anagrams." << endl;
-  }
-  return 0;
+int main()
+{
+  string word1, word2;
+  cout << "Enter first word: ";
+  getline(cin, word1);
+  cout << "Enter second word: ";
+  getline(cin, word2);
+  string isAnagram = checkIsAnagram(word1, word2) ? " are " : " are not ";
+  cout << word1 << " and " << word2 << isAnagram << "anagrams" << endl;
 }
-
