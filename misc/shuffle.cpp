c92f7168cf3819fba4dbbeb880dbd7a9a00857ed
Keen Yee Liau <kyliau@umich.edu>
2015-03-31 21:12:42 -0400

    Update shuffle.cpp

--- misc/shuffle.cpp
+++ misc/shuffle.cpp
@@ -7,37 +7,17 @@
 #include <iostream>
 #include <chrono>
 
-
-typedef std::vector<int>::iterator VectorIt;
-
-void shuffleHelper(VectorIt                    begin,
-                   VectorIt                    end,
-                   std::default_random_engine& generator)
-    // 'begin' points to the first element,
-    // 'end' points to one past the last element
-{
-    if (begin >= end) {
-        return;
-    }
-    size_t numElements = end - begin;
-
-    // This distribution produces random integers in a range [a,b]
-    // where each possible value has an equal likelihood of being produced.
-    std::uniform_int_distribution<int> distribution(0, numElements - 1);
-
-    size_t indexToShuffle = distribution(generator);
-
-    std::swap(*(begin + indexToShuffle), *(end - 1));
-}
-
 void shuffle(std::vector<int>& v)
 {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
-    //std::default_random_engine generator;
-    std::ostream_iterator<int> out_it (std::cout, ", ");
+    
     for (size_t i = 0; i < v.size(); ++i) {
-        shuffleHelper(v.begin(), v.end() - i, generator);
+        std::uniform_int_distribution<int> distribution(i, v.size() - 1);
+        // This distribution produces random integers in a range [a,b]
+        // where each possible value has an equal likelihood of being produced.
+        size_t indexToShuffle = distribution(generator);
+        std::swap(v[i], v[indexToShuffle]);
     }
 }
 
