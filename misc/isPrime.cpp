f9d07c2596edfda1663cfd7a7c6937747802c855
Keen Yee Liau <kyliau@umich.edu>
2015-08-30 21:12:01 -0400

    Create isPrime.cpp

---
+++ misc/isPrime.cpp
@@ -0,0 +1,14 @@
+bool isPrime(int n) {
+  if (n <= 1) {
+    return false;
+  }
+  if (n % 2 == 0) {
+    return n == 2;
+  }
+  for (int i = 3; i * i <= n; i += 2) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
