2ddeef73e6fad6c9c16539f9fa1f78b5e1c08360
Keen <kyliau@umich.edu>
2016-10-27 19:09:40 -0400

    use iterative method for single element problem

--- misc/singleElement.cpp
+++ misc/singleElement.cpp
@@ -36,22 +36,43 @@ int findSingleElement(const vector<int>& v, int left, int right) {
 
 int findSingleElement(const vector<int>& v) {
     assert(!v.empty());
-    return findSingleElement(v, 0, v.size() - 1);
+    int l = 0;
+    int r = v.size() - 1;
+    while (l < r) {
+        assert((r - l) % 2 == 0);   // invariant: array must be of odd length
+        int m = l + (r - l) * 0.5;
+        if (m % 2 == 0) {   // even index, that means left and right half are of even length
+            if (v[m] == v[m - 1]) {
+                r = m - 2;
+            } else {
+                l = m;
+            }
+        } else {            // odd index, that means left and right half are of odd length
+            if (v[m] == v[m - 1]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+    }
+    return v[l];
+    //return findSingleElement(v, 0, v.size() - 1);
 }
 
 int main() {
     // test cases
     using V = vector<int>;
-    V v0 = {1, 1, 2};
-    V v1 = {1, 2, 2};
-    V v2 = {1};
-    V v3 = {1, 1, 2, 2, 3};
-    V v4 = {1, 2, 2, 3, 3};
-    V v5 = {1, 1, 2, 3, 3};
-    V v6 = {1, 1, 2, 2, 3, 4, 4, 5, 5};
-    V v7 = {1, 2, 2, 3, 3, 4, 4, 5, 5};
-    V v8 = {99, -1, -1, 3, 3, 10, 10, -5, -5, 7, 7, 0, 0, 10, 10};
-    V v9 = {-1, -1, 3, 3, 10, 10, -5, -5, 7, 7, 0, 0, 8, 10, 10};
+    V v0  = {1, 1, 2};
+    V v1  = {1, 2, 2};
+    V v2  = {1};
+    V v3  = {1, 1, 2, 2, 3};
+    V v4  = {1, 2, 2, 3, 3};
+    V v5  = {1, 1, 2, 3, 3};
+    V v6  = {1, 1, 2, 2, 3, 4, 4, 5, 5};
+    V v7  = {1, 2, 2, 3, 3, 4, 4, 5, 5};
+    V v8  = {1, 1, 2, 3, 3, 4, 4, 5, 5};
+    V v9  = {1, 1, 2, 2, 3, 3, 4, 5, 5};
+    V v10 = {1, 1, 2, 2, 3, 3, 4, 4, 5};
     vector<V> tests = {v0, v1, v2, v3, v4, v5, v6, v7, v8, v9};
     for (const V& v : tests) {
         cout << "Vector = ";
