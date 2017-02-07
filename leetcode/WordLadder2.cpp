18c5315863299ca171bd7b402000e1e022f85382
Keen <kyliau@umich.edu>
2017-02-07 00:49:04 -0500

    update

--- leetcode/WordLadder2.cpp
+++ leetcode/WordLadder2.cpp
@@ -1,65 +1,105 @@
 // Given two words (beginWord and endWord), and a dictionary's word list,
 // find all shortest transformation sequence(s) from beginWord to endWord,
 // such that:
-
-// * Only one letter can be changed at a time
-// * Each intermediate word must exist in the word list
+// Only one letter can be changed at a time
+// Each transformed word must exist in the word list.
+// Note that beginWord is not a transformed word.
 // For example,
-
 // Given:
 // beginWord = "hit"
 // endWord   = "cog"
-// wordList  = ["hot","dot","dog","lot","log"]
+// wordList  = ["hot","dot","dog","lot","log","cog"]
 // Return
 //   [
 //     ["hit","hot","dot","dog","cog"],
 //     ["hit","hot","lot","log","cog"]
 //   ]
-//   Note:
-//   All words have the same length.
-//   All words contain only lowercase alphabetic characters.
+// Note:
+// Return an empty list if there is no such transformation sequence.
+// All words have the same length.
+// All words contain only lowercase alphabetic characters.
+// You may assume no duplicates in the word list.
+// You may assume beginWord and endWord are non-empty and are not the same.
+
+#include <iostream>
+#include <assert.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+ostream& operator<<(ostream& stream, const vector<string>& v) {
+    stream << "[ ";
+    ostream_iterator<string> it(stream, " ");
+    std::copy(v.begin(), v.end(), it);
+    stream << "]";
+    return stream;
+}
+
+ostream& operator<<(ostream& stream, const vector<vector<string>>& v) {
+    stream << "[ ";
+    for (const auto& s : v) {
+        stream << s << " ";
+    }
+    stream << "]";
+    return stream;
+}
 
 class Solution {
-private:
-    bool isEditDistanceOne(const string& s1, const string& s2) {
-        assert(s1.length() == s2.length());
-        int distance = 0;
-        for (int i = 0; i < s1.length(); ++i) {
-            distance += abs(s1[i] - s2[i]);
-            if (1 < distance) {
-                return false;                                         // RETURN
-            }
-        }
-        return 1 == distance;
-    }
-
 public:
-    vector<vector<string>> findLadders(string                 beginWord,
-                                       string                 endWord,
-                                       unordered_set<string> &wordList) {
-		wordList.insert(beginWord);
-        wordList.insert(endWord);
-
-        unordered_map<string, vector<string&>> map;
-
-        for (const string& s1 : wordList) {
-            for (const string& s2 : wordList) {
-            if (isEditDistance1(s1, s2)) {
-                map[s1].emplace_back(s2);
-                map[s2].emplace_back(s1);
-            }
-        }
-
-        // OK, now I am ready to do the graph search
-        queue<string&> q;
-        q.push(beginWord);
-        while (!q.empty()) {
-            const string& s = q.top();
-            q.pop();
-            for (const string& n : map[s]) {
-
-            }
-        }
-        
+    vector<vector<string>> findLadders(const string&         beginWord,
+                                       const string&         endWord,
+                                       const vector<string>& wordList) {
+        vector<vector<string>> result;
+        return result;
     }
 };
+
+int main() {
+    const struct {
+        int n;
+        string                 beginWord;
+        string                 endWord;
+        vector<string>         wordList;
+        vector<vector<string>> expected;
+    } TEST_CASES[] = {
+        { 1, "hit", "cog",
+            {"hot", "dot", "dog", "lot", "log", "cog"},
+            {
+                {"hit", "hot", "dot", "dog", "cog"},
+                {"hit", "hot", "lot", "log", "cog"},
+            }
+        },
+    };
+    int NUM_CASES = sizeof(TEST_CASES) / sizeof(TEST_CASES[0]);
+    for (int i = 0; i < NUM_CASES; ++i) {
+        const int     n         = TEST_CASES[i].n;
+        const string& beginWord = TEST_CASES[i].beginWord;
+        const string& endWord   = TEST_CASES[i].endWord;
+        const auto&   wordList  = TEST_CASES[i].wordList;
+        const auto&   expected  = TEST_CASES[i].expected;
+        for (int j = 1; j < expected.size(); ++j) {
+            assert(expected[j].size() == expected[j-1].size());
+        }
+        Solution s;
+        const auto result = s.findLadders(beginWord, endWord, wordList);
+        for (int j = 1; j < result.size(); ++j) {
+            assert(result[j].size() == result[j-1].size());
+        }
+        bool isExpected = result.size() == expected.size();
+        for (int j = 0; j < expected.size() && isExpected; ++j) {
+            auto it = std::find(result.begin(), result.end(), expected[j]);
+            isExpected = it != result.end();
+        }
+        cout << "Test case " << n << "\t... ";
+        if (isExpected) {
+            cout << "PASS";
+        } else {
+            cout << "FAIL\n    Expected " << expected << "\n    but got "
+                 << result << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
