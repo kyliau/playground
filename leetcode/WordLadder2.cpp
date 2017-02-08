06ab17a42dc086d4fa62c7eda2b590c30b7afdbb
Keen <kyliau@umich.edu>
2017-02-07 23:30:07 -0500

    update

--- leetcode/WordLadder2.cpp
+++ leetcode/WordLadder2.cpp
@@ -26,8 +26,11 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+#include <stack>
 
 using namespace std;
+using Graph = unordered_map<string, vector<const string *>>;
 
 ostream& operator<<(ostream& stream, const vector<string>& v) {
     stream << "[ ";
@@ -46,12 +49,115 @@ ostream& operator<<(ostream& stream, const vector<vector<string>>& v) {
     return stream;
 }
 
+ostream& operator<<(ostream& stream, const Graph& g) {
+    for (const auto& p : g) {
+        stream << p.first << " : [ ";
+        for (const auto s : p.second) {
+            stream << *s << " ";
+        }
+        stream << "]\n";
+    }
+    return stream;
+}
+
 class Solution {
+private:
+    //using Graph = unordered_map<string, vector<const string *>>;
+    Graph d_graph;
+    Graph d_path;
+
+    bool isEditDistanceOne(const string& s1, const string& s2) {
+        int distance = 0;
+        int length   = s1.length();
+        for (int i = 0; i < length; ++i) {
+            if (s1[i] != s2[i]) {
+                if (distance > 0) {
+                    return false;
+                }
+                ++distance;
+            }
+        }
+        return distance == 1;
+    }
+
+    void buildGraph(const string& beginWord, const vector<string>& wordList) {
+        for (const string& w : wordList) {
+            if (isEditDistanceOne(w, beginWord)) {
+                d_graph[w].emplace_back(&beginWord);
+                d_graph[beginWord].emplace_back(&w);
+            }
+        }
+        for (int i = 0; i < wordList.size(); ++i) {
+            for (int j = i + 1; j < wordList.size(); ++j) {
+                const string& s1 = wordList[i];
+                const string& s2 = wordList[j];
+                if (isEditDistanceOne(s1, s2)) {
+                    d_graph[s1].emplace_back(&s2);
+                    d_graph[s2].emplace_back(&s1);
+                }
+            }
+        }
+    }
+
+    void findShortestDistance(const string& beginWord, const string& endWord) {
+        std::stack<const string *> s;
+        s.push(&beginWord);
+        while (!s.empty()) {
+            const string& w = *(s.top());
+            s.pop();
+            const auto it = d_graph.find(w);
+            assert(it != d_graph.end());
+            const auto& neighbours = it->second;
+            for (const string *n : neighbours) {
+                d_path[*n].emplace_back(&w);
+                bool visited = true;
+                if (!visited) {
+                    s.push(n);
+                }
+            }
+        }
+    }
+
+    void backtrack(vector<vector<string>> *r,
+                   vector<string>         *v,
+                   const string&           word,
+                   const string&           beginWord) {
+        auto it = d_path.find(word);
+        if (d_path.end() == it) {
+            v->emplace_back(beginWord);
+            std::reverse(v->begin(), v->end());
+            return;                                                    // RETURN
+        }
+        v->emplace_back(word);
+        for (int i = 0; i < it->second.size(); ++i) {
+            const string& w = *(it->second[i]);
+            if (i == 0) {
+                backtrack(r, v, w, beginWord);
+            } else {
+                r->emplace_back(*v);
+                backtrack(r, &r->back(), w, beginWord);
+            }
+        }
+    }
+
 public:
     vector<vector<string>> findLadders(const string&         beginWord,
                                        const string&         endWord,
                                        const vector<string>& wordList) {
         vector<vector<string>> result;
+        if (wordList.end() == std::find(wordList.begin(),
+                                        wordList.end(),
+                                        endWord)) {
+            return result;                                             // RETURN
+        }
+        buildGraph(beginWord, wordList);
+        //cout << d_graph;
+        findShortestDistance(beginWord, endWord);
+        auto it = d_path.find(endWord);
+        if (it != d_path.end()) {
+            result.emplace_back();
+            backtrack(&result, &result.back(), endWord, beginWord);
+        }
         return result;
     }
 };
@@ -64,13 +170,25 @@ int main() {
         vector<string>         wordList;
         vector<vector<string>> expected;
     } TEST_CASES[] = {
-        { 1, "hit", "cog",
+        {
+            1,
+            "hit",
+            "cog",
             {"hot", "dot", "dog", "lot", "log", "cog"},
             {
                 {"hit", "hot", "dot", "dog", "cog"},
                 {"hit", "hot", "lot", "log", "cog"},
             }
         },
+        {
+            2,
+            "hot",
+            "got",
+            {"got"},
+            {
+                {"hot", "got"}
+            }
+        }
     };
     int NUM_CASES = sizeof(TEST_CASES) / sizeof(TEST_CASES[0]);
     for (int i = 0; i < NUM_CASES; ++i) {
@@ -97,7 +215,7 @@ int main() {
             cout << "PASS";
         } else {
             cout << "FAIL\n    Expected " << expected << "\n    but got "
-                 << result << endl;
+                 << result;
         }
         cout << endl;
     }
