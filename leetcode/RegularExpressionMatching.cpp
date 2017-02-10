c0db6e46a62ba5fef796c349e2567c8fd2e21d86
Keen <kyliau@umich.edu>
2017-02-10 07:25:36 -0500

    update

--- leetcode/RegularExpressionMatching.cpp
+++ leetcode/RegularExpressionMatching.cpp
@@ -15,13 +15,55 @@
 
 #include <iostream>
 #include <string>
+#include <assert.h>
 
 using namespace std;
 
 class Solution {
 public:
+    int matchStar(const string& s, const string& p, int si, int pi) {
+        int sn = s.length();
+        int pn = p.length();
+        assert(pi < pn - 1);
+        char c = p[pi];
+        assert(p[pi + 1] == '*');
+        if ('.' != c) {
+            while (si != sn && s[si] == c) {
+                ++si;
+            }
+            return si;                                                 // RETURN
+        }
+        bool endOfP = pi == (pn - 2);
+        if (endOfP) {
+            return sn;                                                 // RETURN
+        }
+        char nextC = p[pi + 2];
+        while (si != sn && s[si] != nextC) {
+            ++si;
+        }
+        return si;                                                     // RETURN
+    }
     bool isMatch(const string& s, const string& p) {
-        return false;
+        int si = 0;
+        int sn = s.length();
+        int pn = p.length();
+        for (int pi = 0; pi < pn; ++pi) {
+            if (pi != pn - 1 && p[pi + 1] == '*') {
+                si = matchStar(s, p, si, pi);
+                ++pi;
+            } else {
+                char c = p[pi];
+                if (si == sn) {
+                    return false;
+                }
+                if (c == '.' || c == s[si]) {
+                    ++si;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return si == sn;
     }
 };
 
@@ -32,13 +74,26 @@ int main() {
         string p;
         bool   match;
     } TEST_CASES[] = {
-        {  1,  "aa",     "a", false },
-        {  2,  "aa",    "aa", true  },
-        {  3, "aaa",    "aa", false },
-        {  4,  "aa",    "a*", true  },
-        {  5,  "aa",    ".*", true  },
-        {  6,  "ab",    ".*", true  },
-        {  7, "aab", "c*a*b", true  },
+        // #  string   pattern   match
+        {  1,   "aa",      "a",  false },
+        {  2,   "aa",     "aa",   true },
+        {  3,  "aaa",     "aa",  false },
+        {  4,   "aa",    "aaa",  false },
+        {  5,   "aa",     "a*",   true },
+        {  6,   "aa",     ".*",   true },
+        {  7,   "ab",     ".*",   true },
+        {  8,  "aab",  "c*a*b",   true },
+        {  9,  "abc",    ".*c",   true },
+        { 10,  "abd",    ".*c",  false },
+        { 11,  "aaa",    "a*a",   true },
+        { 12,   "aa",   "ab*a",   true },
+        { 13,  "aba",   "ab*a",   true },
+        { 14, "abba",   "ab*a",   true },
+        { 15,     "",     ".*",   true },
+        { 16,     "",       "",   true },
+        { 17,     "",      ".",  false },
+        { 18,  "aaa",    ".*.",   true },
+        { 19,  "aaa",   ".*.*",   true },
     };
     int NUM_CASES = sizeof(TEST_CASES) / sizeof(TEST_CASES[0]);
     for (int i = 0; i < NUM_CASES; ++i) {
