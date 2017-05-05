c5d19d3970eb1966f5cc720cfe4e516644ce8431
Keen <kyliau@umich.edu>
2017-05-04 23:43:54 -0400

    update

--- leetcode/MergeIntervals.cpp
+++ leetcode/MergeIntervals.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-//using namespace std;
+using namespace std;
 
 /**
  * Definition for an interval.
@@ -17,32 +17,48 @@ struct Interval {
     int end;
     Interval() : start(0), end(0) {}
     Interval(int s, int e) : start(s), end(e) {}
-    //void swap(Interval& lhs, Interval& rhs) {
-    //    std::swap(lhs.start, rhs.start);
-    //    std::swap(lhs.end, rhs.end);
-    //}
 };
-
-//void swap(Interval& lhs, Interval& rhs) {
-//    std::swap(lhs.start, rhs.start);
-//    std::swap(lhs.end, rhs.end);
-//}
 
 bool operator==(const Interval& lhs, const Interval& rhs) {
     return lhs.start == rhs.start && lhs.end == rhs.end;
 }
 
+ostream& operator<<(ostream& stream, const Interval& interval) {
+    return stream << "(" << interval.start << ", " << interval.end << ")";
+}
+
+template <typename T>
+ostream& operator<<(ostream& stream, const vector<T>& v) {
+    stream << "[";
+    for (int i = 0; i < v.size(); ++i) {
+        stream << (i == 0 ? "" : ", ") << v[i];
+    }
+    stream << "]";
+    return stream;
+}
+
 class Solution {
   public:
-    std::vector<Interval> merge(const std::vector<Interval>& intervals) {
-        std::sort(intervals.begin(), intervals.end(), [](const Interval& lhs, const Interval& rhs) {
+    std::vector<Interval> merge(std::vector<Interval>& intervals) {
+        std::sort(intervals.begin(), intervals.end(), [](const Interval& lhs,
+                                                         const Interval& rhs) {
             return lhs.start < rhs.start;
         });
-
-
-
-
         std::vector<Interval> results;
+        if (intervals.empty()) {
+            return results;
+        }
+        Interval interval(intervals[0]);
+        for (int i = 1; i < intervals.size(); ++i) {
+            const Interval& intv = intervals[i];
+            if (intv.start > interval.end) {
+                results.emplace_back(interval);
+                interval = intv;
+            } else {
+                interval.end = std::max(interval.end, intv.end);
+            }
+        }
+        results.emplace_back(interval);
         return results;
     }
 };
@@ -54,11 +70,13 @@ int main() {
         std::vector<Interval> e;
     } CASES[] = {
         { 1, {{1, 3}, {2, 6}, {8, 10}, {15, 18}}, {{1, 6}, {8, 10}, {15, 18}} },
+        { 2, {{1, 2}, {3, 4}, {5, 6}}, {{1, 2}, {3, 4}, {5, 6}} },
+        { 3, {{1, 3}, {4, 5}, {2, 9}, {3, 6}}, {{1, 9}} },
     };
     const int NUM_CASES = sizeof(CASES) / sizeof(CASES[0]);
     for (int i = 0; i < NUM_CASES; ++i) {
         int   n = CASES[i].n;
-        auto& v = CASES[i].v;
+        auto  v = CASES[i].v;
         auto& e = CASES[i].e;
         Solution s;
         std::cout << "Test case " << n << " ... ";
@@ -67,7 +85,7 @@ int main() {
             std::cout << "PASS";
         }
         else {
-            std::cout << "FAIL";
+            std::cout << "FAIL (Expect " << e << " but got " << result << ")";
         }
         std::cout << std::endl;
     }
