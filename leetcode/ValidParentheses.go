77818f94cd7f5d095f590db3e11b4c879834a0bd
Keen Yee Liau <kyliau@umich.edu>
2018-08-03 00:47:41 -0700

    Update ValidParentheses.go

--- leetcode/ValidParentheses.go
+++ leetcode/ValidParentheses.go
@@ -1,3 +1,13 @@
+/*
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * An input string is valid if:
+ *   Open brackets must be closed by the same type of brackets.
+ *   Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ */
+
+// Iterative solution
 func isValid(s string) bool {
   var stack string
   for _, v := range s {
@@ -16,3 +26,49 @@ func isValid(s string) bool {
   }
   return len(stack) == 0
 }
+
+// Recursive solution
+var pairs = map[rune]rune{
+  '(' : ')',
+  '[' : ']',
+  '{' : '}',
+}
+
+// findClosingPair answers the following question:
+// Given an open bracket ( [ or { at position i,
+// return the position of the matching closing bracket.
+// If no such bracket is found, return -1
+func findClosingPair(s string, i int) int {
+  if i >= len(s) {
+    return i
+  }
+  c := rune(s[i]) // rune is like 'char'
+  if strings.ContainsRune(")]}", c) || i >= len(s) - 1 {
+    // 1st condition: Our invariant is that s[i] must be open bracket
+    // 2nd condition: Reached end of string, no way to find matching bracket
+    return -1
+  }
+  // Check the next character
+  next := i + 1
+  // If the next character is an open bracket, recursively find its matching pair
+  for strings.ContainsRune("([{", rune(s[next])) {
+    index := findClosingPair(s, next)
+    if index == -1 || index + 1 >= len(s) {
+      return -1
+    }
+    next = index + 1
+  }
+  if pairs[c] == rune(s[next]) {
+    return next
+  }
+  return -1
+}
+
+func isValid(s string) bool {
+  index := findClosingPair(s, 0)
+  for index != -1 && index + 1 < len(s) {
+    index = findClosingPair(s, index + 1)
+  }
+  return index != -1
+}
+
